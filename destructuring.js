56188a2e8f49320c053c8d817f640e9c84e76890
Alaa Alani <alaaalani19@gmail.com>
2021-07-14 02:18:34 +0300

    add destructuring

---
+++ destructuring.js
@@ -0,0 +1,18 @@
+/*
+  Destructuring
+  -Object
+  -Array
+  -Parameter
+*/
+
+const user ={
+    name: 'Osama,
+    gender: 'Male',
+    age: 36,
+    city: 'cairo',
+    website: 'Elszero',
+    theme: 'Red'
+};
+
+const {name, gender, age, theme = 'Default'} = user;
+console. log('My Name Is: ${name}, Iam $(age), My Theme color Is: ${theme};

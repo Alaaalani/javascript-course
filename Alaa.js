dcaf7410228daaced4a8397a9e569f9d738d5f01
Alaa Alani <alaaalani19@gmail.com>
2021-07-14 02:17:15 +0300

    add alaa

---
+++ Alaa.js
@@ -0,0 +1,18 @@
+/*
+ Template Literals [Template Strings]
+ */
+
+ const thekind = "Male"
+
+ let oldways = "Hello Its ME " +(thekind ==='Male' ? 'Mr' : 'Mrs') + "Osama \n" + 
+ "Iam " + thekind + "\n" +
+ "Iam From Elzero Web School \n " +
+ "I Love All My Follerers";
+
+ let newWay = "Hello Its Me Osama"
+ Iam ${thekind}
+ Iam From Elzero Web School
+ I Love All My Followers ;
+
+ console.log(oldways);
+ console.log(newWay)

f72fd4558e0d0ba753afa3e2f27955e28edf219d
Alaa Alani <alaaalani19@gmail.com>
2021-07-14 02:21:05 +0300

    add to git

---
+++ object.js
@@ -0,0 +1,26 @@
+/*
+  Destructuring
+  - Object
+  - Array
+  - Parameter
+*/
+
+const user = {
+    user: 'Osama',
+    city: iraq
+    skill: {
+        htm1:80% ,
+        CSS: '85%',
+        js: {
+            frameone: 'vuejs',
+            frameone: 'Reactjs',
+            framethree: 'Angularjs'}
+        }
+};
+
+
+const{ username, city, skill:{ htm1, css, js: {frameone: one, frametwo:two, framethree: three}}}
+
+console.log('my name  Is ${user}, and I live in {city}'')
+console.log('my Html skill Progress Is ${htm1}, and {Css}');
+console.log( I have Knowledge in Js Framework Like $ {one}, ${two}, ${three})

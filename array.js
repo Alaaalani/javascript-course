160f0daf71b825f4a9823b996a9e4aef06adb208
Alaa Alani <alaaalani19@gmail.com>
2021-07-14 02:04:48 +0300

    change to array example

--- array.js
+++ array.js
@@ -1,1 +1,22 @@
-console.log("hello Alaa");
+/*
+  - Object 
+  - Array
+  - Parameter
+*/
+
+
+var book = "video",
+    video ="book";
+
+// copy First Variable Into Stash
+var stash = book
+
+//Swapping variable 
+book = video
+
+// Get Book Value From Stash 
+video = stash;
+
+[book, video]
+
+console.log('Book Is ${book} and Video Is ${video}');

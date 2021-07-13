068ff7a78ea2c80c844e66a975480b0e3c0cf44b
Alaa Alani <alaaalani19@gmail.com>
2021-07-14 02:20:12 +0300

    add hit

---
+++ string.js
@@ -0,0 +1,9 @@
+/*
+ New string Metheds 
+ startsWith => string.startsWith(search String, position = 0)
+ endsWith => string.endsWith(search String, Endposition = string.lenght)
+ */
+
+ let x = 'video - Learn How To Learn';
+ console.log( x.endsWith('0', 20));
+ console.log('video - Learn How To '.length);
